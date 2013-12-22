b9dfa9a1395eee98edd716de10432ea8caadb6a2
trollglodita <1trollglodita@gmail.com>
2013-12-22 13:12:19 -0300

    AJAX to zipcode call

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-$.ajax({
-  url: 'http://api.randomuser.me/',
-  dataType: 'json',
-  success: function(data){
-    console.log(data);
-  }
-});
+var randomuserURL = 'http://api.randomuser.me/';
+
+var myButton = document.getElementById('loadbutton');
+myButton.onclick = loadAJAX;
+
+function loadAJAX () {
+	$.ajax({
+	  url: randomuserURL,
+	  dataType: 'json',
+	  success: function(data){
+	    console.log(data.results[0].user.location.zip);
+	  }
+	});
+}
