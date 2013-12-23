25e54b6aac648947021fde2810a558585ec7dd66
trollglodita <1trollglodita@gmail.com>
2013-12-23 15:39:27 -0300

    Geocoder included

--- js/script.js
+++ js/script.js
@@ -3,12 +3,29 @@ var randomuserURL = 'http://api.randomuser.me/';
 var myButton = document.getElementById('loadbutton');
 myButton.onclick = loadAJAX;
 
+var lat = '';
+var lng = '';
+var zipcode;
+
 function loadAJAX () {
 	$.ajax({
 	  url: randomuserURL,
 	  dataType: 'json',
 	  success: function(data){
-	    console.log(data.results[0].user.location.zip);
+	    zipcode = data.results[0].user.location.zip;
+	    latlng();
 	  }
 	});
+}
+
+function latlng () {
+    geocoder.geocode( { 'address': zipcode}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			 lat = results[0].geometry.location.lat();
+			 lng = results[0].geometry.location.lng();
+		} else {
+			alert("Geocode was not successful for the following reason: " + status);
+		}
+	});
+	console.log('Latitude: ' + lat + ' Logitude: ' + lng);
 }
